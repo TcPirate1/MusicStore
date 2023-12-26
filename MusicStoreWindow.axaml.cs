5f14bcc276f766fbd732b5fea5dc4dfa07ff3eff
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-12-26 19:33:40 +1300

    ShowDialog error. No mention of it in tutorial but will finish here anyways. Up to showing dialog.

---
+++ MusicStoreWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicStore
+{
+    public partial class MusicStoreWindow : Window
+    {
+        public MusicStoreWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,23 @@
-﻿namespace MusicStore.ViewModels
+﻿using ReactiveUI;
+using System.Reactive.Linq;
+using System.Windows.Input;
+
+namespace MusicStore.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        public ICommand BuyMusicCommand { get; }
+        public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+        public MainWindowViewModel()
+        {
+            ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
+
+            BuyMusicCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var store = new MusicStoreViewModel();
+
+                var result = await ShowDialog.Handle(store);
+            });
+        }
     }
 }
