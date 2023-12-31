848896d0e1484dc23a1a506b06c25efbac6939d1
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2024-01-01 12:48:09 +1300

    Up to "Album Service" section

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MusicStore
+Following the music store app on Avalonia
+
+## Notes
+https://docs.avaloniaui.net/docs/tutorials/music-store-app/add-content-to-dialog (Diagram of layout of the music store)
+
+https://www.reactiveui.net/docs/handbook/view-models/ (Goes into RaiseAndSetIfChanged, basically is used to detect change, not doing anything with those detections)
+
+https://docs.avaloniaui.net/docs/0.10.x/data-binding/change-notifications#collection-changes (More options for detecting changes)
+
+https://docs.avaloniaui.net/docs/tutorials/music-store-app/mock-search (Uses `ObservableCollection`, a way to detect changes)
--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
 
 namespace MusicStore.ViewModels
 {
-    public class MusicStoreViewModel
+    public class MusicStoreViewModel : ViewModelBase
     {
+        private string? _searchText;
+        private bool _isBusy;
+
+        private string? SearchText
+        {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
+
+        private AlbumViewModel? _selectedAlbum;
+        public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+        public AlbumViewModel? SelectedAlbum
+        {
+            get => _selectedAlbum;
+            set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+        }
+
+        public MusicStoreViewModel()
+        {
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+        }
     }
 }
