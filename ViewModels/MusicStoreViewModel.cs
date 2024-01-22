52c40a0a7fb03bb0b451435d9e712cbfb89d081e
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2024-01-23 11:59:40 +1300

    "Button Data Binding" section

--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading;
@@ -35,14 +36,22 @@ namespace MusicStore.ViewModels
             get => _selectedAlbum;
             set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
         }
+
+        public ReactiveCommand<Unit, AlbumViewModel?> BuyMusicCommand { get; }
         public MusicStoreViewModel()
         {
+            BuyMusicCommand = ReactiveCommand.Create(() =>
+            {
+                return SelectedAlbum;
+            }
+            );
+
             this.WhenAnyValue(x => x.SearchText)
                 .Throttle(TimeSpan.FromMilliseconds(400))
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .Subscribe(DoSearch!);
         }
-       
+        private CancellationTokenSource? _cancellationTokenSource;
         private async void DoSearch(string s)
         {
             IsBusy = true;
@@ -81,7 +90,5 @@ namespace MusicStore.ViewModels
                 }
             }
         }
-
-        private CancellationTokenSource? _cancellationTokenSource;
     }
 }
